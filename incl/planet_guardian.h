148c1e8f105fe9e86c12262e0418243fa40fbe03
Oskari Martela <omartela@c1r1p6.hive.fi>
2025-05-02 21:34:57 +0300

    Adds sprite struct

--- incl/planet_guardian.h
+++ incl/planet_guardian.h
@@ -3,11 +3,19 @@
 
 # include <stdlib.h>
 # include "raylib.h"
+# include "raymath.h"
+# include <time.h>
+
+typedef struct sprite
+{
+    Vector2 pos = {0, 0};
+    Texture2D texture;
+} sprite_t
 
 typedef struct game
 {
     Texture2D planet;
-    Texture2D asteroid;
+    Texture2D asteroid[5];
 } game_t;
 
 extern game_t game;
