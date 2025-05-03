35f3a1fa161e068b1728b95972b8ddf1ad8b4eee
Eve Aviv Keinan <evavkein@gmail.com>
2025-05-03 20:46:57 +0300

    extract main's code into various files and functions

--- incl/planet_guardian.h
+++ incl/planet_guardian.h
@@ -2,21 +2,23 @@
 # define PLANET_GUARDIAN_H
 
 # include <stdlib.h>
+# include <stdio.h>
+# include <time.h>
 # include "raylib.h"
 # include "raymath.h"
-# include <time.h>
-# include <stdio.h>
+
+# define SCREEN_WIDTH 1000
+# define SCREEN_HEIGHT 1000
 
 # define MAX_ASTEROIDS 10
 # define MAX_ASTEROID_SPEED 5.0f
 # define MIN_ASTEROID_RADIUS 10.0f
 # define MAX_ASTEROID_RADIUS 20.0f
 # define GRAVITYPULL_SCALE 0.000005f
+
 # define TEMP_CHANGE 10 
 # define MAX_TEMP 255
 # define MIN_TEMP 0
-
-void draw_shield(float angle);
 
 typedef struct sprite
 {
@@ -28,6 +30,12 @@ typedef struct sprite
     bool        is_inside_screen;
 	float       speed;
 } sprite_t;
+
+typedef struct shield {
+    sprite_t   sprite;
+    float angle;
+    int direction;
+} shield_t;
 
 typedef struct shadow {
     Texture2D   texture;
@@ -48,7 +56,7 @@ typedef struct cold {
 typedef struct game
 {
     sprite_t   planet;
-    sprite_t   shield;
+    shield_t   shield;
     sprite_t   asteroid[MAX_ASTEROIDS];
     cold_t     cold;
     shadow_t   shadow;
@@ -57,6 +65,12 @@ typedef struct game
 } game_t;
 
 extern game_t game;
+
+void gameplay_frame();
+
+void textureLoader();
+void textureUnload();
+
 
 void printp(char *msg, float x, float y, float size, Color color);
 Vector2 moveTowardsWithGravity(Vector2 current, Vector2 direction, int speed, Vector2 planetCenter);
@@ -67,10 +81,9 @@ sprite_t initialize_asteroid(sprite_t asteroid);
 Vector2 get_center_pos(sprite_t sprite);
 bool check_collision(sprite_t sprite_a, sprite_t sprite_b);
 void draw_asteroids();
+void draw_shield(float angle);
 void check_colliding_asteroids();
 void initialize_out_of_bounds_asteroids();
 void	update_planet_condition(float delta_time, float shieldAngle);
-
-
 
 #endif
---
+++ src/gameplay_frame.c
@@ -0,0 +1,102 @@
+#include "planet_guardian.h"
+
+void gameplay_frame()
+{
+	// --- Calculate Shield Collision Circles ---
+	float orbitRadius = game.planet.radius + 50.0f;
+	Vector2 planetCenter = game.planet.center_pos;
+	Vector2 shieldMidPoint = {
+		planetCenter.x + cosf(game.shield.angle) * orbitRadius,
+		planetCenter.y + sinf(game.shield.angle) * orbitRadius
+	};
+	Vector2 tangentDir = {-sinf(game.shield.angle), cosf(game.shield.angle)};
+
+	// --- Shield Circle Parameters (TUNABLE) ---
+	float shieldSegmentOffset = game.shield.sprite.texture.width * 0.4f;
+	float shieldSegmentRadius = game.shield.sprite.texture.width * 0.15;
+	Vector2 shieldCircle1Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, shieldSegmentOffset));
+	Vector2 shieldCircle2Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, -shieldSegmentOffset));
+	shieldSegmentRadius = game.shield.sprite.texture.width * 0.093f;
+	Vector2 shieldCircle3Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, -shieldSegmentOffset * 0.2));
+	Vector2 shieldCircle4Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, shieldSegmentOffset * 0.2));
+	shieldSegmentRadius = game.shield.sprite.texture.width * 0.12f;
+	Vector2 shieldCircle5Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, -shieldSegmentOffset * 0.7));
+	Vector2 shieldCircle6Center = Vector2Add(shieldMidPoint, Vector2Scale(tangentDir, shieldSegmentOffset * 0.7));
+
+
+	if (IsKeyPressed(KEY_SPACE))
+			game.shield.direction = -game.shield.direction;
+	float deltaTime = GetFrameTime();
+	game.shield.angle += deltaTime * 3.0f * game.shield.direction; // Get time elapsed since last frame
+	// if (IsKeyDown(KEY_LEFT))
+	// 	shieldAngle -= deltaTime * 3.0f;  // Get time elapsed since last frame
+
+	// Initialize asteroids
+	// float spawnDelay = 3.0f;  // Delay in seconds before spawning new asteroid
+	// float totalTime = 0.0f;  // Total elapsed time
+	// int asteroid1;
+	// int asteroid2;
+	int i = 0;
+	while (i < MAX_ASTEROIDS)
+	{
+		game.asteroid[i].center_pos.x = game.asteroid[i].pos.x + game.asteroid[i].radius;
+		game.asteroid[i].center_pos.y = game.asteroid[i].pos.y + game.asteroid[i].radius;
+		if (CheckCollisionCircles(game.planet.center_pos, game.planet.radius, game.asteroid[i].center_pos, game.asteroid[i].radius))
+			game.asteroid[i] = initialize_asteroid(game.asteroid[i]);
+		game.asteroid[i].pos = moveTowardsWithGravity(game.asteroid[i].pos, game.planet.center_pos, game.asteroid[i].speed, game.planet.center_pos);
+
+		if (CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle1Center, shieldSegmentRadius) ||
+		CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle2Center, shieldSegmentRadius) ||
+		CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle3Center, shieldSegmentRadius)||
+		CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle4Center, shieldSegmentRadius)||
+		CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle5Center, shieldSegmentRadius)||
+		CheckCollisionCircles(game.asteroid[i].center_pos, game.asteroid[i].radius, shieldCircle6Center, shieldSegmentRadius))
+		{
+				game.asteroid[i] = initialize_asteroid(game.asteroid[i]);
+		}
+		i++;
+	}
+	initialize_out_of_bounds_asteroids();
+	check_colliding_asteroids();
+		// float deltaTime = GetFrameTime();  // Get time elapsed since last frame
+		// totalTime += deltaTime;
+
+	BeginDrawing();
+		ClearBackground(BLACK);
+
+
+
+		DrawTexturePro(game.background, (Rectangle){.x = 0, .y = 0,
+				.width = game.background.width, .height = game.background.height},
+			(Rectangle){.x = game.background.width, .y = game.background.height,
+				.width = SCREEN_WIDTH, .height = SCREEN_HEIGHT},
+			(Vector2){.x = game.background.width, .y = game.background.height},
+			0,
+			WHITE);
+
+		update_planet_condition(deltaTime, game.shield.angle);
+		DrawTextureV(game.planet.texture, game.planet.pos, WHITE);
+		DrawTextureV(game.cold.texture, game.planet.pos,
+			(Color){.r = 255, .g = 255, .b = 255, .a = game.cold.value});
+
+		DrawTexturePro(game.shadow.texture,
+			(Rectangle){.x = 0, .y = 0,
+				.height = game.shadow.texture.height, .width = game.shadow.texture.width},
+			(Rectangle){.x = SCREEN_WIDTH / 2, .y = SCREEN_HEIGHT / 2,
+				.height = game.shadow.texture.height, .width = game.shadow.texture.width},
+			(Vector2){.x = game.planet.texture.width / 2, .y = game.planet.texture.height / 2},
+			game.shadow.rotation,
+			(Color){.r = 0, .g = 0, .b = 0, .a = 150});
+		draw_shield(game.shield.angle);
+		// printf("%f\n", remainder(shieldAngle, 3.6));
+		//debugging shield
+		// DrawCircleLinesV(shieldCircle1Center, game.shield.texture.width * 0.093f, BLACK);
+		// DrawCircleLinesV(shieldCircle2Center, game.shield.texture.width * 0.093f, BLACK);
+		// DrawCircleLinesV(shieldCircle3Center, game.shield.texture.width * 0.15f, BLACK);
+		// DrawCircleLinesV(shieldCircle4Center, game.shield.texture.width * 0.15f, BLACK);
+		// DrawCircleLinesV(shieldCircle5Center, game.shield.texture.width * 0.12f, BLACK);
+		// DrawCircleLinesV(shieldCircle6Center, game.shield.texture.width * 0.12f, BLACK);
+		draw_asteroids();
+		printp("HEALTH: 3", 550., 750., 1.0f, RED);
+	EndDrawing();
+}
