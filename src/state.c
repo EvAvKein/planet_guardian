f47fea430283a305aa9100963432af3c923b6239
Joonas Tuomi <jtuomi@student.hive.fi>
2025-05-03 21:54:35 +0300

    Added state.c file

---
+++ src/state.c
@@ -0,0 +1,12 @@
+#include "planet_guardian.h"
+
+void drawMenu() {
+    DrawTextureRec(game.menu, (Rectangle){40, 200, 120, 200}, (Vector2){0, 0}, WHITE); 
+    printp("PRESS SPACE", 600, 300, 1.0f, RED);
+    if(IsKeyPressed(KEY_SPACE))
+        game.state = GAME;
+}
+
+void deathScreen() {
+
+}
