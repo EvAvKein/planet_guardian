b0b62569760e5533efc96b1579fb51407b14067d
Julius Pellinen <jpelline@c1r1p5.hive.fi>
2025-05-04 13:31:29 +0300

    Rebase

--- src/sounds.c
+++ src/sounds.c
@@ -1,7 +1,16 @@
 #include "planet_guardian.h"
 
+void soundLoader()
+{
+    game.sound.shield = LoadSound("assets/audio/shield_hit.mp3");
+    game.sound.theme = LoadSound("assets/audio/theme.mp3");
+}
 void play_shield_sound()
 {
-    Sound shield = LoadSound("assets/audio/shield_hit.mp3");
-    PlaySound(shield);
+    PlaySound(game.sound.shield);
+}
+
+void playTheme()
+{
+    PlaySound(game.sound.theme);
 }
